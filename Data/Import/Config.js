2aa5c26d0169d75bef2a79bcf8b54f2a12689320
tcsbmogarage <61685580+tcsbmogarage@users.noreply.github.com>
2020-03-25 09:15:18 -0500

    Update Config.js
    Points to Bankz

--- Data/Import/Config.js
+++ Data/Import/Config.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyCZyZDAFw_FSB6RLp-pAGuPcHq28J6NJ54",
-    authDomain: "bmo-world.firebaseapp.com",
-    databaseURL: "https://bmo-world.firebaseio.com",
-    projectId: "bmo-world",
-    storageBucket: "bmo-world.appspot.com",
-    messagingSenderId: "99155845796",
-    appId: "1:99155845796:web:230864643fcc23ea31fc31",
-    measurementId: "G-R5VGKLE2SZ"
+    apiKey: "AIzaSyCEa6WLvmJ6YNnmwUPGLTQe5EtR5aEGwzM",
+    authDomain: "bmo-bankz.firebaseapp.com",
+    databaseURL: "https://bmo-bankz.firebaseio.com",
+    projectId: "bmo-bankz",
+    storageBucket: "bmo-bankz.appspot.com",
+    messagingSenderId: "688258315434",
+    appId: "1:688258315434:web:d4d9ad965fe5e4136e8cd3",
+    measurementId: "G-9FPRKJ8FLM"
   };
 
   module.exports = firebaseConfig;
